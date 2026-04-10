bdd3199d15f685ea756b3d152eaf1d1e4d0022d8
Tannidey97 <rktanni7@gmail.com>
2026-04-10 23:34:34 +0600

    Create three_num.C

---
+++ three_num.C
@@ -0,0 +1,12 @@
+//Program : Read three numbers from the keyboard and print them using conversion specifiers.
+
+#include<stdio.h>
+int main()
+{
+	int jk,rm,jin;
+	printf("enter three integers : ");
+	scanf("%d %d %d",&jk,&rm,&jin);
+	
+	printf("1st num : %d\n2nd num : %d\n3rd num : %d\n",jk,rm, jin);
+	return 0;
+}
